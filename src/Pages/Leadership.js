11b6e660d8404bfe326935032ad6dcefaebb7f1f
Sinit Gebrehiwet <stgebrehiwet@wisc.edu>
2025-10-11 15:02:01 -0500

    Added Chairs

--- src/Pages/Leadership.js
+++ src/Pages/Leadership.js
@@ -30,14 +30,6 @@ export  function Leadership() {
                 <Card id='eboard' image={Kamila} name="Kamila Patris" title="Vice President"/>
                 <Card id='eboard' image={Wynton} name="Wynton Wright" title="Treasurer"/>
                 <Card id='eboard' image={Claire} name="Claire Neblett" title="Secretary"/>
-                <Card id='eboard' image={Jeremy} name="Jeremy Moore" title="Pre-Collegiate Initiative Chair"/>
-                <Card id='eboard' image={Aaron} name="Aaron Dent" title="Academic & Professional Excellence Chair"/>
-                <Card id='eboard' image={Mulu} name="Mulu Yosief" title="Social Chair"/>
-                <Card id='eboard' image={Aiden} name="Aiden Williams" title="Social Chair"/>
-                <Card id='eboard' image={Cornell} name="Cornell Stewart" title="Fundraising Chair"/>
-                <Card id='eboard' image={George} name="George Walker" title="T.O.R.C.H. Chair"/>
-                <Card id='eboard' image={Sinit} name="Sinit Gebrehiwet" title="Webmaster Chair"/>
-                <Card id='eboard' image={Jeremie} name="Jeremie Peart" title="Outreach Chair"/>
                 <Card id='eboard' image={Jaynika} name="Jaynika Williams" title="Senator"/>
                 <Card id='eboard' image={DeMario} name="DeMario Randle" title="Senator"/>
                 <Card id='eboard' image={Wilfred} name="Wilfred Shereni" title="President Emeritus"/>
@@ -53,9 +45,14 @@ export function Chairs() {
     return (
         <>
             <div className="Chairs">
-                    <Card id='eboard' image={Julius} name="Julius Nurse" title="Outreach Chair"/>
-                    <Card id='eboard' image={Kofo} name="Kofo Aminu" title="Fundraising Chair"/>
-                    <Card id='eboard' image={Marie} name="Marie Jobe" title="Volunteer Chair"/>
+                <Card id='eboard' image={Jeremy} name="Jeremy Moore" title="Pre-Collegiate Initiative Chair"/>
+                <Card id='eboard' image={Aaron} name="Aaron Dent" title="Academic & Professional Excellence Chair"/>
+                <Card id='eboard' image={Mulu} name="Mulu Yosief" title="Social Chair"/>
+                <Card id='eboard' image={Aiden} name="Aiden Williams" title="Social Chair"/>
+                <Card id='eboard' image={Cornell} name="Cornell Stewart" title="Fundraising Chair"/>
+                <Card id='eboard' image={George} name="George Walker" title="T.O.R.C.H. Chair"/>
+                <Card id='eboard' image={Sinit} name="Sinit Gebrehiwet" title="Webmaster Chair"/>
+                <Card id='eboard' image={Jeremie} name="Jeremie Peart" title="Outreach Chair"/>
             </div>
         </>
     )
