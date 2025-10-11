b07b12229ff202de54cb9eca162d15402d9214c8
Sinit Gebrehiwet <stgebrehiwet@wisc.edu>
2025-10-11 16:52:04 -0500

    Updated Oct Events

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -8,21 +8,20 @@ export default function Events() {
             <>
                  <div className="Events">
                     <p id='title'>Events</p>
-                    {/*<p id="month">March</p>*/}
-                    <p id="month">Febuary</p>
-                    <div className="Febuary">
-                        <Event id='event' day="Wed" date="26" eventName="InterPro Professional Developement Presentation" time="6:00PM" location="2255 Engineering Hall"/>
-                        <Event id='event' day="Thu" date="20" eventName="Meet The Professional" time="6:00PM" location="Tripp Commons"/>
-                        <Event id='event' day="Thu" date="13" eventName="Heritage Trivia Night" time="5:30PM" location="1413 Cheney Room Engineering Hall"/>
-                        <Event id='event' day="Tue" date="11" eventName="3D Printing & laser Cutting Valentines Workshop" time="4:00PM" location="Makerspace, Wendt Commons"/>
-                        <Event id='event' day="Thu" date="6" eventName="Career Fair Prep Night" time="6:00PM" location="1800 Engineering Hall"/>
-
+                    
+                    <p id="month">October</p>
+                    <div className="October">
+                        <Event id='event' day="Fri" date="3" eventName="GE Healthcare Site-Visit" time="2:30PM" location="GE Healthcare Madison Factory"/>
+                        <Event id='event' day="Fri" date="10" eventName="Findorff Site-Visit" time="2:30PM" location="Center for Black Excellence & Culture"/>
+                        <Event id='event' day="Wed" date="15" eventName="PCI Kick-Off" time="?" location="LaFollette High School"/>
+                        <Event id='event' day="Mon" date="20" eventName="Blood Drive Fundraiser" time="?" location="Library Mall"/>
                     </div>
-                    <p id="month">January</p>
-                    <div className="January">
+                    
+                    {/*<p id="month">September</p>
+                    <div className="September">
                         <Event id='event' day="Wed" date="29" eventName="General Body Meeting" time="6:00PM" location="1413 Cheney Room Engineering Hall"/>
 
-                    </div>
+                    </div>*/}
                  </div>   
             </>
     );
