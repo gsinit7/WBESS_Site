4032544f3a8e62a4105930ea8bb29c6710e292a9
Sinit Gebrehiwet <stgebrehiwet@wisc.edu>
2026-02-03 10:15:44 -0600

    Added New Link

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -15,59 +15,36 @@ export default function Gallery() {
             <>
                  <div className="Gallery">
                     <div className="GalleryIntro">
-                        <p id="title">Posts Gallery</p>
-                        <p id="subtext">Come see what we have been up to</p>
+                        <p id="title">Gallery</p>
+                        <p id="subtext">Come see what we have been up to!</p>
                     </div>                    
                     <div>
                         <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
                             <Masonry>
-
+                                                                   
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBcjxT8P_A-/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DQNK_yPjkRi/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DCSCyo3PyIf/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DQDPkT2joyy/" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DCH38BzsVu2/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DQe9kvOjngo/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DCFWdPevO4V/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DQNK_yPjkRi/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DB913cephuH/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DPT6drojuIr/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DB7Zp8uPttV/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DOmdHzBjgjd/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DB4zrCAPBZY/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DB1fR-Rv6xQ/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBzpVGOvdOF/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBxDeT-PavN/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBuSVUWsHIX/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBrxffovZBM/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBe43Ngv6FM/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBcGtLLPpif/" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DBZiqLmPu6B/" width={328} />
-                                </div>
-
+                                   <InstagramEmbed url="https://www.instagram.com/p/DJSlJ8PvhEU/?img_index=1" width={328} />
+                                </div> 
+                                
+                                
                             </Masonry>
                         </ResponsiveMasonry>
                     </div>
