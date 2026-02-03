c83b7727ceadd506bd74d3b8a9d848c75912212c
Sinit Gebrehiwet <stgebrehiwet@wisc.edu>
2026-02-03 13:18:05 -0600

    Copied the Code

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -17,30 +17,15 @@ export default function Gallery() {
                     <div className="GalleryIntro">
                         <p id="title">Gallery</p>
                         <p id="subtext">Come see what we have been up to!</p>
-                    </div>                    
+                    </div>
                     <div>
                         <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
                             <Masonry>
-                                                                   
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-<<<<<<< HEAD
-                                   <InstagramEmbed url="https://www.instagram.com/p/DQDPkT2joyy/" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DR7wTItjh8R/?img_index=1/" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DQe9kvOjngo/?img_index=1" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DQNK_yPjkRi/?img_index=1" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DPT6drojuIr/?img_index=1" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-                                   <InstagramEmbed url="https://www.instagram.com/p/DOmdHzBjgjd/?img_index=1" width={328} />
-                                </div>
-                                <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
-=======
-                                   <InstagramEmbed url="https://www.instagram.com/p/DQNK_yPjkRi/?img_index=1" width={328} />
+                                   <InstagramEmbed url="https://www.instagram.com/p/DRAQWd0Dv3c/" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
                                    <InstagramEmbed url="https://www.instagram.com/p/DQDPkT2joyy/" width={328} />
@@ -58,7 +43,6 @@ export default function Gallery() {
                                    <InstagramEmbed url="https://www.instagram.com/p/DOmdHzBjgjd/?img_index=1" width={328} />
                                 </div>
                                 <div id='post' style={{ display: 'flex', justifyContent: 'center' }}>
->>>>>>> 4032544f3a8e62a4105930ea8bb29c6710e292a9
                                    <InstagramEmbed url="https://www.instagram.com/p/DJSlJ8PvhEU/?img_index=1" width={328} />
                                 </div> 
                                 
